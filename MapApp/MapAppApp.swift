af48de47965753b79b980e976a65453952acb453
Lori Rothermel <lori@rothermel1.com>
2024-11-27 17:53:13 -0800

    Initial Commit

---
+++ MapApp/MapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapAppApp.swift
+//  MapApp
+//
+//  Created by Lori Rothermel on 11/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
