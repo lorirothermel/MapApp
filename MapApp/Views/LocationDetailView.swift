26c3b40e9021c6111d2c0827806de5c40eef4de3
Lori Rothermel <lori@rothermel1.com>
2024-12-01 18:58:35 -0800

    #7 Use Sheet to Create a Detail View for Locatons

---
+++ MapApp/Views/LocationDetailView.swift
@@ -0,0 +1,18 @@
+//
+//  LocationDetailView.swift
+//  MapApp
+//
+//  Created by Lori Rothermel on 12/1/24.
+//
+
+import SwiftUI
+
+struct LocationDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    LocationDetailView()
+}
